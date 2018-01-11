4f98f9f91268a2cb7d98fe8e74e13033778f3616
kebudke <31251507+kebudke@users.noreply.github.com>
2018-01-11 10:13:34 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,11 +2,11 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def render_main():
     return render_template('home.html')
 
-@app.route("/response", methods=['POST'])
+@app.route('/response', methods=['POST', 'GET'])
 def render_response():
     color = request.form['color']
     if color == 'turquoise':
